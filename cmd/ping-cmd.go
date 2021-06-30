f685256ca1fb3089393d29d9de971ddb79e2a287
Never12581 <azaizai@icloud.com>
2021-06-30 23:01:59 +0800

    cmd ping improve

--- cmd/ping-cmd.go
+++ cmd/ping-cmd.go
@@ -7,14 +7,22 @@ import (
 )
 
 type PingCmd struct {
+	value []byte
 }
 
 func (p *PingCmd) Init(bs [][]byte) error {
+	if bs != nil && len(bs) >= 1 {
+		p.value = bs[0]
+	}
 	return nil
 }
 
 func (p *PingCmd) Deal(ctx context.Context) result.ResultInter {
 	bss := make([][]byte, 0)
-	bss = append(bss, constant.Pong)
+	if p.value == nil {
+		bss = append(bss, constant.Pong)
+	} else {
+		bss = append(bss, p.value)
+	}
 	return result.SuccessResult(bss)
 }
