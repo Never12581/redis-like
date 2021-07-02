adffe46ad8a58b1a42b536e56c4ab749934c3f96
Never12581 <azaizai@icloud.com>
2021-07-02 22:16:13 +0800

    update

--- storage/storage.go
+++ storage/storage.go
@@ -2,6 +2,13 @@ package storage
 
 import (
 	"context"
+	"fmt"
+	"github.com/coocood/freecache"
+	"github.com/syndtr/goleveldb/leveldb"
+	"log"
+	"redis-like/config"
+	"runtime/debug"
+	"sync"
 )
 
 type ErrorInfo string
@@ -20,4 +27,48 @@ type Storage interface {
 	Get(context context.Context, key []byte) ([]byte, error)
 	Set(context context.Context, key []byte, value []byte) error
 	Close()
+}
+
+var (
+	once    sync.Once
+	storage Storage
+)
+
+func NewError(text ErrorInfo, engine string, err error) error {
+	var errorInfo string
+	if err != nil {
+		errorInfo = err.Error()
+	}
+	return fmt.Errorf(string(text), engine, errorInfo)
+}
+
+func StorageInstance() Storage {
+	once.Do(newStorage)
+	return storage
+}
+
+func newStorage() {
+
+	envMode := config.EnvConfigInstance()
+
+	engine, _ := envMode.Engine()
+	switch engine {
+	case config.LevelDBEngine:
+
+		storagePath, _ := envMode.StoragePath()
+		db, err := leveldb.OpenFile(storagePath, nil)
+		if err != nil {
+			log.Println(err)
+			panic(err)
+		}
+		storage = NewLevelDB(db)
+
+	case config.FreeCacheEngine:
+
+		freeCacheSize, _ := envMode.FreeCacheEngineSize()
+		cache := freecache.NewCache(freeCacheSize)
+		debug.SetGCPercent(20)
+		storage = NewFreeCache(cache)
+	}
+
 }
